64df6182dbdc37a57a515e6f98c7a1ef49b4d067
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-12-09 15:15:31 +0700

    add user role by admin

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,32 +1,41 @@
-import {Body, Controller, Get, Post, UseGuards} from "@nestjs/common";
+import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
 import {CreateUserDto} from "./dto/create-user.dto";
 import {UsersService} from "./users.service";
 import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {User} from "./users.model";
-import {JwtAuthGuard} from "../auth/jwt-auth.guard";
 import {Roles} from "../auth/roles-auth.decorator";
+import {AddRoleDto} from "./dto/add-role.dto";
 import {RolesGuard} from "../auth/roles-guard";
 
 @ApiTags('Users')
 @Controller('users')
 export class UsersController {
 
-  constructor(private userService: UsersService) {}
+  constructor(private usersService: UsersService) {
+  }
 
-  @ApiOperation({summary: "User creation"})
+  @ApiOperation({summary: 'User creation'})
   @ApiResponse({status: 200, type: User})
   @Post()
   create(@Body() userDto: CreateUserDto) {
-    return this.userService.createUser(userDto);
+    return this.usersService.createUser(userDto);
   }
 
-  @ApiOperation({summary: "Get all users"})
+  @ApiOperation({summary: 'Get all users'})
   @ApiResponse({status: 200, type: [User]})
-  // @UseGuards(JwtAuthGuard)
   @Roles("Admin")
   @UseGuards(RolesGuard)
   @Get()
   getAll() {
-    return this.userService.getAllUsers();
+    return this.usersService.getAllUsers();
+  }
+
+  @ApiOperation({summary: 'Give role'})
+  @ApiResponse({status: 200})
+  @Roles("Admin")
+  @UseGuards(RolesGuard)
+  @Post('/role')
+  addRole(@Body() dto: AddRoleDto) {
+    return this.usersService.addRole(dto);
   }
 }
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,8 +1,9 @@
-import {Injectable} from "@nestjs/common";
+import {HttpException, HttpStatus, Injectable} from "@nestjs/common";
 import {User} from "./users.model";
 import {InjectModel} from "@nestjs/sequelize";
 import {CreateUserDto} from "./dto/create-user.dto";
 import {RolesService} from "../roles/roles.service";
+import {AddRoleDto} from "./dto/add-role.dto";
 
 @Injectable()
 export class UsersService {
@@ -24,5 +25,15 @@ export class UsersService {
 
   async getUserByEmail(email: string) {
     return await this.userRepository.findOne({where: {email}, include: {all: true}})
+  }
+
+  async addRole(dto: AddRoleDto) {
+    const user = await this.userRepository.findByPk(dto.userId);
+    const role = await this.roleService.getRoleByValue(dto.value);
+    if (role && user) {
+      await user.$add('role', role.id);
+      return dto;
+    }
+    throw new HttpException('user or role is not found', HttpStatus.NOT_FOUND);
   }
 }
