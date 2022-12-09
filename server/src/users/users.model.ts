14c42cc30a915ee685c46b1aa4dd212d922fb8b3
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-12-09 13:40:44 +0700

    user roles table

--- server/src/users/users.model.ts
+++ server/src/users/users.model.ts
@@ -1,14 +1,14 @@
-import {Column, DataType, Model, Table} from "sequelize-typescript";
+import {BelongsToMany, Column, DataType, Model, Table} from "sequelize-typescript";
 import {ApiProperty} from "@nestjs/swagger";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles.model";
 
 interface UserCreationAttrs {
   email: string;
   password: string;
 }
 
-@Table({
-  tableName: 'users'
-})
+@Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttrs> {
   @ApiProperty({example: '1', description: 'unique identification'})
   @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
@@ -21,4 +21,7 @@ export class User extends Model<User, UserCreationAttrs> {
   @ApiProperty({example: 'secret_password', description: 'password'})
   @Column({type: DataType.STRING, allowNull: false})
   password: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[];
 }
