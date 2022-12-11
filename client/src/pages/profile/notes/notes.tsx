68a6dca4e2c0e31f000b13936a2fd43cec40ec7a
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2022-12-11 17:12:56 +0700

    simple accordion with little redesign

--- client/src/pages/profile/notes/notes.tsx
+++ client/src/pages/profile/notes/notes.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./notes.module.scss";
 
 export const Notes = () => {
+  const [todos, setTodos] = useState();
+
   return (
     <div className={styles.container}>
       <h1>Notes</h1>
